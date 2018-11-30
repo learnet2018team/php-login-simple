638f79782f0ca27ac328ae82427e065bd4e9d848
learnet2018team <42828558+learnet2018team@users.noreply.github.com>
2018-11-30 01:09:34 -0500

    Update database.php

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 
-$server = 'localhost:3307';
-$username = 'root';
-$password = '';
-$database = 'php_login_database';
+$server = 'us-cdbr-iron-east-01.cleardb.net';
+$username = 'b629f283022924';
+$password = 'e1c7c3a3';
+$database = 'heroku_d65ac763a69ff84';
 
 try {
   $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
